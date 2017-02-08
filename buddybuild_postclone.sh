13a8f7d2807d9c2b29fe0589f9980dd0b63305ff
Marc Weistroff <marc@weistroff.net>
2017-02-08 15:28:35 +0100

    change bundle id again and again

--- buddybuild_postclone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Change the bundle id
-echo "Changing the bundle id of Info.plist at path $BUDDYBUILD_WORKSPACE/ios/Spir/Info.plist"
-/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier net.delatech.ios.spir" "$BUDDYBUILD_WORKSPACE/ios/Spir/Info.plist"
