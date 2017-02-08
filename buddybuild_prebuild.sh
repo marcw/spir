d82f45972ccf266e58b62bcfbc42eba9713ef447
Romain Pouclet <romain.pouclet@gmail.com>
2017-02-08 09:05:11 -0500

    Update buddybuild_prebuild.sh

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Change the bundle id
+echo "Changing the bundle id of Info.plist at path $BUDDYBUILD_WORKSPACE/ios/Spir/Info.plist"
 /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier net.delatech.ios.spir" "$BUDDYBUILD_WORKSPACE/ios/Spir/Info.plist"
